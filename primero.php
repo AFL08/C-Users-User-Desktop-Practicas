cc87c789abfb077054391f10058a690bd32f549c
AFL08 <alferrloba@gmail.com>
2018-09-25 09:22:40 +0200

    Inicio de nuevo

--- primero.php
+++ primero.php
@@ -1,5 +1,36 @@
 
 <?php
-    print "Hola mundo";
 
+    print "Hola mundo";
+    class Heroes
+    {
+        public $nombreHeroe;
+        public $poderHeroe;
+        public $descripcionHeroe;
+        public $universoHeroe;
+
+        public function __construct(){
+            
+        }
+        
+
+        public function __construct($nombreHeroe, $poderHeroe, $descripcionHeroe, $universoHeroe){
+            $this->nombreHeroe=$nombreHeroe;
+            $this->poderHeroe=$poderHeroe;
+            $this->descripcionHeroe=$descripcionHeroe;
+            $this->universoHeroe=$universoHeroe;
+        }
+
+        public function ComprobacionUniversoPerteneciente(){
+            if($univeroHeroe = "DC"){
+                echo "Este personaje pertenece al universo DC"
+            }else if($universoHeroe = "Marvel"){
+                echo "Este personaje pertenece al universo Marvel"
+            }else{
+                echo "Este personaje pertenece a cualquier universo diferente"
+            }
+        }
+
+    }
+    
 ?>
