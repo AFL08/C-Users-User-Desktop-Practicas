bf69c077351b823f0b35b6f121bdf77808ccdd51
AFL08 <alferrloba@gmail.com>
2018-09-25 13:44:04 +0200

    Inicio Actualizacion 5

--- nucleo.php
+++ nucleo.php
@@ -8,10 +8,9 @@
         $heroeRegistrado = new Heroes("Iron Man","Fuerza y Vuelo"
         ,"Multimillonario, Prodigio creando armas, Con la armadura puede destruir cualquier cosa y consigue reponerse a cualquier problema"
         ,"Marvel");
-        
+
         foreach($heroeRegistrado as $hero){
-            #echo sprintf("<h1>%s</h1>",$hero->nombre);
-            echo "<h1>".$hero."</h1></br>";
+            echo "<h1>$hero</h1></br>";
         }
 ?>
 </body>
