da93352026e5f9ac77ae005de483745da79c20c0
AFL08 <alferrloba@gmail.com>
2018-09-26 10:11:05 +0200

    Iniciar Actualizacion 1.1

--- nucleo.php
+++ nucleo.php
@@ -21,12 +21,22 @@
         foreach($heroeRegistrado as $hero){
             #echo "<p>" . $hero . "</p><br/>";
             #if($heroeRegistrado=array($IronMan=array([]))
-            var_dump($hero);
+            ?>
+            <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title"><?php $hero->nombreHeroe?></h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </div>
+
+            <?php
+            #var_dump($hero);
             #$hero->ComprobacionUniversoPerteneciente($hero->get_universoHeroe());
         }
-
-
 ?>
+
 
 
 </body>
