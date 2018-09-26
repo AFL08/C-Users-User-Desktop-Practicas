26217ac4117146cb229a7e943d7352dad86354c5
AFL08 <alferrloba@gmail.com>
2018-09-26 10:30:57 +0200

    Iniciar Actualizacion 1.1

--- nucleo.php
+++ nucleo.php
@@ -35,7 +35,7 @@
                 <p class="card-text">Poder SuperHeroe: <?php echo $hero->poderHeroe?></p>
                 <p class="card-text">Detalles SuperHeroe: <?php echo $hero->detallesHeroe?></p>
                 <p class="card-text">Universo SuperHeroe: <?php echo $hero->universoHeroe?></p>
-                <img class="card-img-top" src="" alt="Card image cap">
+                <img class="card-img-top" src="" alt="Card image cap" height="100px">
                 <!--<a href="#" class="btn btn-primary">Go somewhere</a>-->
             </div>
             </div>
