ea138f9540f85006d4c3bdb50bbe11fa993a726f
AFL08 <alferrloba@gmail.com>
2018-09-26 10:12:14 +0200

    Iniciar Actualizacion 1.1

--- nucleo.php
+++ nucleo.php
@@ -23,7 +23,7 @@
             #if($heroeRegistrado=array($IronMan=array([]))
             ?>
             <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+            <!--<img class="card-img-top" src=".../100px180/" alt="Card image cap">-->
             <div class="card-body">
                 <h5 class="card-title"><?php $hero->nombreHeroe?></h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
