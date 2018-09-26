245cbb84eecef6a2c6579381e5d1a0196bb81c80
AFL08 <alferrloba@gmail.com>
2018-09-26 13:23:05 +0200

    Iniciar Actualizacion 1.2

--- ConexionBaseDatos.php
+++ ConexionBaseDatos.php
@@ -5,7 +5,7 @@
             echo "<p>Hola Mundo</p>";
             $conexionBaseDatos = mysql_connect("localhost","alvaro","alvaro");
             if(!$conexionBaseDatos){
-                die("No se pudo conectar a la base de datos" . mysql_error());
+                echo "<p>No se pudo conectar a la base de datos" . mysql_error() . "</p>";
             }
             echo '<p>Conexion exitosa</p>';
             mysql_close();
