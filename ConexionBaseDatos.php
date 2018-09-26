b0eab73416e4281ddaf7b15e7ace294a6ec42f98
AFL08 <alferrloba@gmail.com>
2018-09-26 14:10:19 +0200

    Iniciar Actualizacion 1.2

--- ConexionBaseDatos.php
+++ ConexionBaseDatos.php
@@ -4,12 +4,12 @@
 
         public function ComprobarConexion(){
             echo "<p>Hola Mundo</p>";
-            $conexionBaseDatos = mysql_connect('192.168.1.144','alvaro','alvaro','HeroesRegistrados');
-            /*if(!$conexionBaseDatos){
-                echo "<p>No se pudo conectar a la base de datos" . mysql_error() . "</p>";
-            }*/
+            $conexionBaseDatos = mysqli_connect('localhost','alvaro','alvaro','HeroesRegistrados',3306);
+            if(!$conexionBaseDatos){
+                echo "<p>No se pudo conectar a la base de datos" . mysqli_error() . "</p>";
+            }
             echo "<p>Conexion exitosa</p>";
-            mysql_close();
+            mysqli_close();
         }
         
         
