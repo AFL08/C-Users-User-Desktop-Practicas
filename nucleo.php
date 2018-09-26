66f507fac92b97ae67be13de4faf5211407cadaf
AFL08 <alferrloba@gmail.com>
2018-09-26 09:45:23 +0200

    Iniciar Actualizacion 1.1

--- nucleo.php
+++ nucleo.php
@@ -7,7 +7,7 @@
     <input type="button" name="mostrarDatosHeroe"/><br/>-->
 
 <?php
-    include 'primero.php';
+        /*include 'primero.php';
         $IronMan = new Heroes("Iron Man","Fuerza y Vuelo"
         ,"Multimillonario, Prodigio creando armas, Con la armadura puede destruir cualquier cosa y consigue reponerse a cualquier problema"
         ,"Marvel");
@@ -16,15 +16,15 @@
         ,"Telarañas"
         ,"Marvel");
 
-        $heroeRegistrado = array($IronMan,$Spiderman);
+        $heroeRegistrado = array($IronMan,$Spiderman);*/
 
         echo "<p>hola</p>";
-        foreach($heroeRegistrado as $hero){
+        /*foreach($heroeRegistrado as $hero){
             #echo "<p>" . $hero . "</p><br/>";
             #if($heroeRegistrado=array($IronMan=array([]))
             var_dump($hero);
             $IronMan->ComprobacionUniversoPerteneciente($IronMan->get_universoHeroe());
-        }
+        }*/
 
 
 ?>
