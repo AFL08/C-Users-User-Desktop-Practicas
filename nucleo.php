76eb7d45ad4493b109bf25e051e1ea970a2ade39
AFL08 <alferrloba@gmail.com>
2018-09-26 16:28:23 +0200

    Iniciar Actualizacion 1.5

--- nucleo.php
+++ nucleo.php
@@ -49,9 +49,9 @@
 
             <?php
             #var_dump($hero);
-            mysqli_query("INSERT INTO Heroe (nombre, poder, detalles, universo) values ('$hero->nombreHeroe','$hero->poderHeroe','$hero->detallesHeroe'
-            ,'$hero->universoHeroe');");
-            printf('Has registrado al heroe con la identificacion: %d\n', mysqli_insert_id());
+            mysqli_query("INSERT INTO Heroe (nombre, poder, detalles, universo) values (" . $hero->nombreHeroe . "," . $hero->poderHeroe . "," . $hero->detallesHeroe . "
+            ," . $hero->universoHeroe . ");");
+            printf('Has registrado al heroe con la identificacion: %d', mysqli_insert_id());
             $realizacionConexion->CerrarConexion();
             
         }
